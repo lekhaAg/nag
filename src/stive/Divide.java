ec88437d021c9811dff122f1d8bc6ba6c4b3702b
lekha <lekha@pcomputer>
2019-03-01 22:43:27 +0530

    divideoperation

---
+++ src/stive/Divide.java
@@ -0,0 +1,16 @@
+package stive;
+
+public class Divide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		int a=30;
+		int b=2;
+		int z;
+		 z=a/b;
+		 System.out.println(z);
+		
+
+	}
+
+}
