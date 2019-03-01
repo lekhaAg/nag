46bdc4110b36039964716cfcc2e28e49b9a43517
lekha <lekha@pcomputer>
2019-03-01 20:40:03 +0530

    addition

---
+++ src/stive/Addp.java
@@ -0,0 +1,16 @@
+package stive;
+
+public class Addp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x,y ,z;
+        x=10;
+        y=20;
+        z=x+y;
+        System.out.println(z);
+        
+
+	}
+
+}
